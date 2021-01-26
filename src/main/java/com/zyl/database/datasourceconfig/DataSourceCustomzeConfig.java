97128b3c31441bc2defa74a4e6256d384c75f125
zyl271174152 <271174152@qq.com>
2021-01-26 10:07:49 +0800

    数据源yml配置信息

--- src/main/java/com/zyl/database/datasourceconfig/DataSourceCustomzeConfig.java
+++ src/main/java/com/zyl/database/datasourceconfig/DataSourceCustomzeConfig.java
@@ -2,6 +2,8 @@ package com.zyl.database.datasourceconfig;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +19,7 @@ import java.util.Map;
  * @description
  */
 @Configuration
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class DataSourceCustomzeConfig {
 
     @Bean("mysql")
@@ -28,12 +31,46 @@ public class DataSourceCustomzeConfig {
         return build;
     }
 
-    @Bean("oracle")
-    @ConfigurationProperties("spring.datasource.test")
-    public DataSource DataSourceOracle() {
-        DruidDataSource build = DruidDataSourceBuilder.create().build();
-        return build;
-    }
+//    @Bean("oracle")
+//    @ConfigurationProperties("spring.datasource.test")
+//    public DataSource DataSourceOracle() {
+//        DruidDataSource build = DruidDataSourceBuilder.create().build();
+//        return build;
+//    }
+
+//    //配置Druid的监控
+//    //1、配置一个管理后台的Servlet
+//    @Bean
+//    public ServletRegistrationBean statViewServlet(){
+//        ServletRegistrationBean bean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+//        Map<String,String> initParams = new HashMap<>();
+//
+//        initParams.put("loginUsername","admin");
+//        initParams.put("loginPassword","123456");
+//        initParams.put("allow","");//默认就是允许所有访问
+//        initParams.put("deny","192.168.15.21"); //IP黑名单 (存在共同时，deny优先于allow) : 如果满足deny的话提示:Sorry, you are not permitted to view this page.
+//        initParams.put("resetEnable","false");  //是否能够重置数据.
+//
+//        bean.setInitParameters(initParams);
+//        return bean;
+//    }
+
+
+//    //2、配置一个web监控的filter
+//    @Bean
+//    public FilterRegistrationBean webStatFilter(){
+//        FilterRegistrationBean bean = new FilterRegistrationBean();
+//        bean.setFilter(new WebStatFilter());
+//
+//        Map<String,String> initParams = new HashMap<>();
+//        initParams.put("exclusions","*.js,*.css,/druid/*");
+//
+//        bean.setInitParameters(initParams);
+//
+//        bean.setUrlPatterns(Arrays.asList("/*"));
+//
+//        return  bean;
+//    }
 
 
     /**
@@ -44,11 +81,10 @@ public class DataSourceCustomzeConfig {
     @Bean(name = "dynamicDataSource")
     @Primary
     public DynamicDataSource dataSource() {
-        Map<Object, Object> targetDataSources = new HashMap<>()
-;
+        Map<Object, Object> targetDataSources = new HashMap<>();
         targetDataSources.put("mysql", DataSourceMysql());
-                targetDataSources.put("oracle", DataSourceOracle());
-                return new DynamicDataSource(DataSourceOracle(), targetDataSources);
-                }
+//        targetDataSources.put("oracle", DataSourceOracle());
+        return new DynamicDataSource(DataSourceMysql(), targetDataSources);
+    }
 
-                }
+}
--- src/main/java/com/zyl/database/datasourceconfig/RestTemplateConfig.java
+++ src/main/java/com/zyl/database/datasourceconfig/RestTemplateConfig.java
@@ -2,14 +2,14 @@ package com.zyl.database.datasourceconfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 import java.nio.charset.StandardCharsets;
 
 /**
- *自定义restTemplate
+ * 自定义restTemplate
  */
 @Configuration
 public class RestTemplateConfig {
@@ -18,18 +18,19 @@ public class RestTemplateConfig {
 
     /**
      * '@LoadBalanced'注解表示使用Ribbon实现客户端负载均衡
+     *
      * @return RestTemplate
      */
     @Bean
     public RestTemplate restTemplate() {
         if (restTemplate == null) {
-            HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+            SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
             factory.setReadTimeout(10000);
             //ms
             factory.setConnectTimeout(5000);
             //ms
             restTemplate = new RestTemplate(factory);
-            restTemplate.getMessageConverters().set(1,new StringHttpMessageConverter(StandardCharsets.UTF_8));
+            restTemplate.getMessageConverters().set(1, new StringHttpMessageConverter(StandardCharsets.UTF_8));
         }
         return restTemplate;
     }
